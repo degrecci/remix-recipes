be9b24e455b8634785bf7fb761b238d65a43680b
Henrique Degrecci <hdegrecci@gmail.com>
2023-08-12 20:15:46 -0300

    Add form and button

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,30 @@
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
 import type { V2_MetaFunction } from "@remix-run/node";
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Remix Recipes" },
+    { name: "description", content: "Welcome to Remix Recipes!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1 className="font-bold text-3xl mb-4">Hello world</h1>
+    <div className="container py-6">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
+        Remix Recipes
+      </h1>
+      <div className="flex w-full mx-auto mt-14 items-center space-x-2 justify-center">
+        <Input
+          className="md:w-2/5"
+          type="text"
+          placeholder="Type a recipe name"
+        />
+        <Button className="min-w-fit" type="submit">
+          Find my recipe
+        </Button>
+      </div>
     </div>
   );
 }
